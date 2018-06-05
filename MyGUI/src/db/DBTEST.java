2f2fceb3f36382b8a1a6a87f3784086328242010
moritz-eliasschultze <moritz-eliasschultze@AMS-GYM-EDV1-13.gym.ams.local>
2018-06-05 15:48:51 +0200

    MES

---
+++ MyGUI/src/db/DBTEST.java
@@ -0,0 +1,20 @@
+package db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class DBTEST {
+	public static void main(String[] args) {
+		try {
+		new DBTEST();
+		}catch (SQLExceptionn e) {
+			e.printStackTrace();
+		}
+}
+	public DBTEST() throws SQLException {
+		Connection conn = DriverManager.getConnection("jdbc:ucanaccess://C://Users//moritz-eliasschultze/DBTest.accdb;memory=false");
+		
+		Statement stmt = conn.createStatement();
+		stmt.execute("INSERT INTO address (vorname,nachname,ort,plz)" + "VALUES ('hansi','maier'")
+	}
+}
